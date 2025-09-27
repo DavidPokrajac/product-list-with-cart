228c3b54bec9a86e6a2317e8aca472aa85f8c7d1
David <d0pokrajac@gmail.com>
2025-09-27 13:28:06 +0200

    Created your-cart component that will display items in your shopping cart

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCartList } from './product-cart-list/product-cart-list';
+import { YourCart } from './your-cart/your-cart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductCartList],
+  imports: [RouterOutlet, ProductCartList, YourCart],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/your-cart/your-cart.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-cart',
+  imports: [],
+  templateUrl: './your-cart.html',
+  styleUrl: './your-cart.scss'
+})
+export class YourCart {
+
+}
