5a8994e6833b651dae2f58fdfa437f6033115104
David <d0pokrajac@gmail.com>
2025-10-22 08:24:22 +0200

    Applied active styles, added some animations

--- src/app/product-cart/product-cart.ts
+++ src/app/product-cart/product-cart.ts
@@ -3,7 +3,8 @@ import { Component, inject, Input, OnInit, signal, computed } from '@angular/cor
 import { AddToCart } from '../add-to-cart';
 import { RemoveFromCart } from '../remove-from-cart';
 import { CartModel } from '../models/CartModel';
-
+import { gsap } from 'gsap/gsap-core';
+gsap.registerPlugin();
 @Component({
   selector: 'app-product-cart',
   imports: [NgClass, CurrencyPipe],
@@ -65,42 +66,135 @@ export class ProductCart implements OnInit {
     }
   }
 
-  handleQuantityDecrement(name: string) {
-    this.cartItems.update((items: any) => {
-      return items
-        .filter((item: any) => item.name === name)
-        .map((i: any, index: number) => {
-          if (i.quantity === 1) {
-            this.removedItem.set(name);
-            this.cartItems().splice(index, 1);
-            this.itemToRemove.removeFromCart$(this.removedItem());
-            return this.cartItems;
-          }
-          i.quantity = i.quantity - 1;
-          this.addToYourCart.addToCart$({
-            name: this.name,
-            quantity: i.quantity,
-            price: this.price,
-          });
-          return i;
-        });
+  handleQuantityDecrement(event: Event, name: string) {
+    const hi = (event.target as Element)
+      ?.closest('.quantity-handler')
+      ?.querySelector('.item-quantity') as HTMLElement;
+    const timeline = gsap.timeline();
+    timeline.to(hi, {
+      x: -100,
+      duration: 0.75,
+      ease: 'elastic.inOut(1, 0.5)',
     });
+    timeline.to(
+      hi,
+      {
+        opacity: 0,
+        duration: 0.5,
+        ease: 'power4.inOut',
+      },
+      '-=0.65'
+    );
+    timeline.to(hi, {
+      opacity: 0,
+      x: 125,
+      duration: 0.25,
+      ease: 'elastic.inOut(1, 0.35)',
+    });
+    timeline.to(hi, {
+      opacity: 1,
+      x: 0,
+      duration: 0.35,
+      ease: 'elatic.inOut(1, 0.25)',
+    });
+    setTimeout(() => {
+      this.cartItems.update((items: any) => {
+        return items
+          .filter((item: any) => item.name === name)
+          .map((i: any, index: number) => {
+            if (i.quantity === 1) {
+              this.removedItem.set(name);
+              this.cartItems().splice(index, 1);
+              this.itemToRemove.removeFromCart$(this.removedItem());
+              return this.cartItems;
+            }
+            i.quantity = i.quantity - 1;
+            this.addToYourCart.addToCart$({
+              name: this.name,
+              quantity: i.quantity,
+              price: this.price,
+            });
+            return i;
+          });
+      });
+    }, 1000);
   }
 
-  handleQuantityIncrement(name: string) {
-    this.cartItems.update((items: any) => {
-      return items
-        .filter((item: any) => item.name === name)
-        .map((i: any) => {
-          i.quantity = i.quantity + 1;
-          this.addToYourCart.addToCart$({
-            name: this.name,
-            quantity: i.quantity,
-            price: this.price,
-          });
-          return i;
-        });
+  handleQuantityIncrement(event: Event, name: string) {
+    const hi = (event.target as Element)
+      ?.closest('.quantity-handler')
+      ?.querySelector('.item-quantity') as HTMLElement;
+    const timeline = gsap.timeline();
+    timeline.to(hi, {
+      x: 100,
+      duration: 0.75,
+      ease: 'elastic.inOut(1, 0.5)',
     });
+    timeline.to(
+      hi,
+      {
+        opacity: 0,
+        duration: 0.4,
+        ease: 'power4.inOut',
+      },
+      '-=0.55'
+    );
+    timeline.to(hi, {
+      opacity: 0,
+      x: -125,
+      duration: 0.25,
+      ease: 'elastic.inOut(1, 0.75)',
+    });
+    timeline.to(hi, {
+      opacity: 1,
+      x: 0,
+      duration: 0.5,
+      ease: 'elastic.inOut(1, 0.75)',
+    });
+    setTimeout(() => {
+      this.cartItems.update((items: any) => {
+        return items
+          .filter((item: any) => item.name === name)
+          .map((i: any) => {
+            i.quantity = i.quantity + 1;
+            this.addToYourCart.addToCart$({
+              name: this.name,
+              quantity: i.quantity,
+              price: this.price,
+            });
+            return i;
+          });
+      });
+    }, 1000);
+  }
+
+  handleEnterAnimation(event: Event) {
+    const hi = (event.target as HTMLElement).querySelector('.add-to-cart-icon');
+    gsap.to(hi, {
+      scale: 1.2,
+      duration: 0.75,
+      ease: 'power2.inOut',
+    });
+    gsap
+      .to(hi, {
+        rotate: '-30deg',
+        duration: 0.2,
+        ease: 'power2.inOut',
+        repeat: 3,
+        yoyo: true,
+      })
+      .play();
+  }
+
+  handleLeaveAnimation(event: Event) {
+    const hi = (event.target as HTMLElement).querySelector('.add-to-cart-icon');
+    gsap
+      .to(hi, {
+        scale: '1',
+        duration: 0.5,
+        ease: 'power2.inOut',
+      })
+      .play();
   }
 
   itemQuantity = computed(() => {
